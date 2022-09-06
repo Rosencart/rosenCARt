eef95f1f5eefa8e7a660d9643b4f2f38bc40c270
Justin Rosengart <justinrosengart@gmail.com>
2022-09-06 12:20:03 +0200

    Update main.py

---
+++ main.py
@@ -0,0 +1,319 @@
+import os
+import random
+import warnings
+
+import numpy
+import numpy as np
+
+import pandas as pd
+import tensorflow as tf
+import matplotlib.pyplot as plt
+from keras.models import load_model
+from keras.utils import to_categorical
+from keras.layers import Conv2D, Dense, Flatten, MaxPool2D, Dropout
+
+import tkinter as tk
+from tkinter import *
+from tkinter import filedialog
+
+from matplotlib.image import imread
+
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+
+from PIL import ImageTk, Image
+
+
+data_dir = 'recourse'
+train_path = 'recourse/Train'
+test_path = 'recourse/Test'
+
+# Resizing the images to 30x30x3
+IMG_HEIGHT = 30
+IMG_WIDTH = 30
+channels = 3
+
+NUM_CATEGORIES = len(os.listdir(train_path))
+NUM_CATEGORIES
+
+# Label Overview
+classes = {0: 'Speed limit (20km/h)',
+           1: 'Speed limit (30km/h)',
+           2: 'Speed limit (50km/h)',
+           3: 'Speed limit (60km/h)',
+           4: 'Speed limit (70km/h)',
+           5: 'Speed limit (80km/h)',
+           6: 'End of speed limit (80km/h)',
+           7: 'Speed limit (100km/h)',
+           8: 'Speed limit (120km/h)',
+           9: 'No passing',
+           10: 'No passing veh over 3.5 tons',
+           11: 'Right-of-way at intersection',
+           12: 'Priority road',
+           13: 'Yield',
+           14: 'Stop',
+           15: 'No vehicles',
+           16: 'Veh > 3.5 tons prohibited',
+           17: 'No entry',
+           18: 'General caution',
+           19: 'Dangerous curve left',
+           20: 'Dangerous curve right',
+           21: 'Double curve',
+           22: 'Bumpy road',
+           23: 'Slippery road',
+           24: 'Road narrows on the right',
+           25: 'Road work',
+           26: 'Traffic signals',
+           27: 'Pedestrians',
+           28: 'Children crossing',
+           29: 'Bicycles crossing',
+           30: 'Beware of ice/snow',
+           31: 'Wild animals crossing',
+           32: 'End speed + passing limits',
+           33: 'Turn right ahead',
+           34: 'Turn left ahead',
+           35: 'Ahead only',
+           36: 'Go straight or right',
+           37: 'Go straight or left',
+           38: 'Keep right',
+           39: 'Keep left',
+           40: 'Roundabout mandatory',
+           41: 'End of no passing',
+           42: 'End no passing veh > 3.5 tons'}
+
+folders = os.listdir(train_path)
+
+train_number = []
+class_num = []
+
+for folder in folders:
+    train_files = os.listdir(f'{train_path}/' + folder)
+    train_number.append(len(train_files))
+    class_num.append(classes[int(folder)])
+
+# Sorting the dataset on the basis of number of images in each class
+zipped_lists = zip(train_number, class_num)
+sorted_pairs = sorted(zipped_lists)
+tuples = zip(*sorted_pairs)
+train_number, class_num = [list(tuple) for tuple in tuples]
+
+# Plotting the number of images in each class
+plt.figure(figsize=(21, 10))
+plt.bar(class_num, train_number)
+plt.xticks(class_num, rotation='vertical')
+plt.show()
+
+# Visualizing 25 random images from test data
+
+test = pd.read_csv(f'{data_dir}/Test.csv')
+imgs = test["Path"].values
+
+plt.figure(figsize=(25, 25))
+
+for i in range(1, 26):
+    plt.subplot(5, 5, i)
+    random_img_path = f'{data_dir}/' + random.choice(imgs)
+    rand_img = imread(random_img_path)
+    plt.imshow(rand_img)
+    plt.grid(visible=None)
+    plt.xlabel(rand_img.shape[1], fontsize=20)  # width of image
+    plt.ylabel(rand_img.shape[0], fontsize=20)  # height of image
+
+imgs_path = "recourse/Train"
+data_list = []
+labels_list = []
+classes_list = 43
+for i in range(classes_list):
+    i_path = os.path.join(imgs_path, str(i))  # 0-42
+    for img in os.listdir(i_path):
+        im = Image.open(i_path + '/' + img)
+        im = im.resize((30, 30))
+        im = np.array(im)
+        data_list.append(im)
+        labels_list.append(i)
+data = np.array(data_list)
+labels = np.array(labels_list)
+print("Done")
+
+path = "recourse/Train/0/00000_00004_00029.png"
+img = Image.open(path)
+img = img.resize((30, 30))
+sr = np.array(img)
+plt.imshow(img)
+plt.show()
+
+
+x_train, x_test, y_train, y_test = train_test_split(data, labels, test_size=0.1, random_state=10)
+
+print("training_shape: ", x_train.shape, y_train.shape)
+print("testing_shape: ", x_test.shape, y_test.shape)
+
+y_train = tf.one_hot(y_train, 43)
+y_test = tf.one_hot(y_test, 43)
+
+training_shape: (35288, 30, 30, 3)
+testing_shape: (3921, 30, 30, 3)
+
+model = tf.keras.Sequential()
+
+model.add(Conv2D(filters=32, kernel_size=(5, 5), activation="relu", input_shape=x_train.shape[1:]))
+model.add((Conv2D(filters=64, kernel_size=(5, 5), activation="relu")))
+model.add(MaxPool2D(pool_size=(2, 2)))
+model.add(Dropout(rate=0.25))
+model.add((Conv2D(filters=128, kernel_size=(3, 3), activation="relu")))
+model.add((MaxPool2D(pool_size=(2, 2))))
+model.add(Dropout(rate=0.25))
+model.add(Flatten())
+model.add(Dense(1024, activation="relu"))
+model.add(Dropout(rate=0.40))
+model.add(Dense(43, activation="softmax"))
+
+model.summary()
+
+model.compile(loss="categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
+
+epochs = 5
+history = model.fit(x_train, y_train, epochs=epochs, batch_size=64, validation_data=(x_test, y_test))
+
+plt.figure(0)
+plt.plot(history.history['accuracy'], label="Training accuracy")
+plt.plot(history.history['val_accuracy'], label="val accuracy")
+plt.title("Accuracy Graph")
+plt.xlabel("epochs")
+plt.ylabel("accuracy (0,1)")
+plt.legend()
+
+plt.figure(1)
+plt.plot(history.history['loss'], label="training loss")
+plt.plot(history.history['val_loss'], label="val loss")
+plt.title("Loss Graph")
+plt.xlabel("epochs")
+plt.ylabel("Loss")
+plt.legend()
+plt.show()
+
+
+test = pd.read_csv("recourse/Test.csv")
+test_labels = test['ClassId'].values
+test_img_path = "recourse"
+test_imgs = test['Path'].values
+
+test_data = []
+test_labels = []
+
+for img in test_imgs:
+    im = Image.open(f'{test_img_path}/' + img)
+    im = im.resize((30, 30))
+    im = np.array(im)
+    test_data.append(im)
+
+test_data = np.array(test_data)
+print(test_data.shape)
+
+
+warnings.filterwarnings("ignore")
+test_labels = test['ClassId'].values
+test_labels
+
+# predictions = model.predict_classes(test_data)
+predictions = np.argmax(model.predict(test_data), axis=1)
+print("accuracy: ", accuracy_score(test_labels, predictions))
+
+model.save('traffic_signal_classifier.h5')
+
+
+model = load_model('./traffic_signal_classifier.h5')
+# dictionary to label all traffic signs class.
+classes = {1: 'Speed limit (20km/h)',
+           2: 'Speed limit (30km/h)',
+           3: 'Speed limit (50km/h)',
+           4: 'Speed limit (60km/h)',
+           5: 'Speed limit (70km/h)',
+           6: 'Speed limit (80km/h)',
+           7: 'End of speed limit (80km/h)',
+           8: 'Speed limit (100km/h)',
+           9: 'Speed limit (120km/h)',
+           10: 'No passing',
+           11: 'No passing veh over 3.5 tons',
+           12: 'Right-of-way at intersection',
+           13: 'Priority road',
+           14: 'Yield',
+           15: 'Stop',
+           16: 'No vehicles',
+           17: 'Veh > 3.5 tons prohibited',
+           18: 'No entry',
+           19: 'General caution',
+           20: 'Dangerous curve left',
+           21: 'Dangerous curve right',
+           22: 'Double curve',
+           23: 'Bumpy road',
+           24: 'Slippery road',
+           25: 'Road narrows on the right',
+           26: 'Road work',
+           27: 'Traffic signals',
+           28: 'Pedestrians',
+           29: 'Children crossing',
+           30: 'Bicycles crossing',
+           31: 'Beware of ice/snow',
+           32: 'Wild animals crossing',
+           33: 'End speed + passing limits',
+           34: 'Turn right ahead',
+           35: 'Turn left ahead',
+           36: 'Ahead only',
+           37: 'Go straight or right',
+           38: 'Go straight or left',
+           39: 'Keep right',
+           40: 'Keep left',
+           41: 'Roundabout mandatory',
+           42: 'End of no passing',
+           43: 'End no passing veh > 3.5 tons'}
+# initialise GUI
+top = tk.Tk()
+top.geometry('800x600')
+top.title('Traffic sign classification')
+top.configure(background='#CDCDCD')
+label = Label(top, background='#CDCDCD', font=('arial', 15, 'bold'))
+sign_image = Label(top)
+
+
+def classify(file_path):
+    global label_packed
+    image = Image.open(file_path)
+    image = image.resize((30, 30))
+    image = numpy.expand_dims(image, axis=0)
+    image = numpy.array(image)
+    pred = model.predict_classes([image])[0]
+    sign = classes[pred + 1]
+    print(sign)
+    label.configure(foreground='#011638', text=sign)
+
+
+def show_classify_button(file_path):
+    classify_b = Button(top, text="Classify Image", command=lambda: classify(file_path), padx=10, pady=5)
+    classify_b.configure(background='#364156', foreground='white', font=('arial', 10, 'bold'))
+    classify_b.place(relx=0.79, rely=0.46)
+
+
+def upload_image():
+    try:
+        file_path = filedialog.askopenfilename()
+        uploaded = Image.open(file_path)
+        uploaded.thumbnail(((top.winfo_width() / 2.25), (top.winfo_height() / 2.25)))
+        im = ImageTk.PhotoImage(uploaded)
+        sign_image.configure(image=im)
+        sign_image.image = im
+        label.configure(text='')
+        show_classify_button(file_path)
+    except:
+        pass
+
+
+upload = Button(top, text="Upload an image", command=upload_image, padx=10, pady=5)
+upload.configure(background='#364156', foreground='white', font=('arial', 10, 'bold'))
+upload.pack(side=BOTTOM, pady=50)
+sign_image.pack(side=BOTTOM, expand=True)
+label.pack(side=BOTTOM, expand=True)
+heading = Label(top, text="Know Your Traffic Sign", pady=20, font=('arial', 20, 'bold'))
+heading.configure(background='#CDCDCD', foreground='#364156')
+heading.pack()
+top.mainloop()
