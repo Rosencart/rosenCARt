932aa9dca938204ec748c858438326362b94b8f6
Justin Rosengart <justinrosengart@gmail.com>
2022-09-07 12:04:04 +0200

    Update traffic_sign.py
    optimized imports

--- traffic_sign.py
+++ traffic_sign.py
@@ -1,15 +1,13 @@
+import os
+
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-import cv2
-import tensorflow as tf
 from PIL import Image
-import os
-from sklearn.model_selection import train_test_split
-from keras.utils import to_categorical
-from keras.models import Sequential, load_model
 from keras.layers import Conv2D, MaxPool2D, Dense, Flatten, Dropout
-from sklearn.metrics import accuracy_score
+from keras.models import Sequential
+from keras.utils import to_categorical
+from sklearn.model_selection import train_test_split
 
 data = []
 labels = []
