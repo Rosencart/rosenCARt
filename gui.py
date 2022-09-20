16aac35a0a367317f6ba4776a907a63f2b25895e
Justin Rosengart <justinrosengart@gmail.com>
2022-09-20 17:16:38 +0200

    Update gui.py

--- gui.py
+++ gui.py
@@ -1,3 +1,5 @@
+from settings import Settings
+
 import tkinter as tk
 from tkinter import *
 from tkinter import filedialog
@@ -10,51 +12,6 @@ from keras.models import load_model
 class GUI:
     def __init__(self):
         self.model = load_model('traffic_signal_classifier.h5')
-
-        # dictionary to label all traffic signs class.
-        self.classes = {1: 'Speed limit (20km/h)',
-                        2: 'Speed limit (30km/h)',
-                        3: 'Speed limit (50km/h)',
-                        4: 'Speed limit (60km/h)',
-                        5: 'Speed limit (70km/h)',
-                        6: 'Speed limit (80km/h)',
-                        7: 'End of speed limit (80km/h)',
-                        8: 'Speed limit (100km/h)',
-                        9: 'Speed limit (120km/h)',
-                        10: 'No passing',
-                        11: 'No passing veh over 3.5 tons',
-                        12: 'Right-of-way at intersection',
-                        13: 'Priority road',
-                        14: 'Yield',
-                        15: 'Stop',
-                        16: 'No vehicles',
-                        17: 'Veh > 3.5 tons prohibited',
-                        18: 'No entry',
-                        19: 'General caution',
-                        20: 'Dangerous curve left',
-                        21: 'Dangerous curve right',
-                        22: 'Double curve',
-                        23: 'Bumpy road',
-                        24: 'Slippery road',
-                        25: 'Road narrows on the right',
-                        26: 'Road work',
-                        27: 'Traffic signals',
-                        28: 'Pedestrians',
-                        29: 'Children crossing',
-                        30: 'Bicycles crossing',
-                        31: 'Beware of ice/snow',
-                        32: 'Wild animals crossing',
-                        33: 'End speed + passing limits',
-                        34: 'Turn right ahead',
-                        35: 'Turn left ahead',
-                        36: 'Ahead only',
-                        37: 'Go straight or right',
-                        38: 'Go straight or left',
-                        39: 'Keep right',
-                        40: 'Keep left',
-                        41: 'Roundabout mandatory',
-                        42: 'End of no passing',
-                        43: 'End no passing veh > 3.5 tons'}
 
         # initialise GUI
         self.top = tk.Tk()
@@ -82,7 +39,7 @@ class GUI:
         image = numpy.array(image)
         print(image.shape)
         pred = self.model.predict([image])[0]
-        sign = self.classes[pred + 1]
+        sign = Settings.classes[pred + 1]
         print(sign)
         self.label.configure(foreground='#011638', text=sign)
 
