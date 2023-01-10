fdab9dd01501cfb0e8dd4d82963f79106bf21eb6
Justin Rosengart <justinrosengart@gmail.com>
2023-01-10 10:40:47 +0100

    Update model.py

--- model.py
+++ model.py
@@ -23,28 +23,28 @@ model.compile(optimizer='adam',
               metrics=['accuracy'])
 
 # Define the data generators
-train_datagen = ImageDataGenerator(rescale = 1./255,
-                                   shear_range = 0.2,
-                                   zoom_range = 0.2,
-                                   horizontal_flip = True)
+train_datagen = ImageDataGenerator(rescale=1. / 255,
+                                   shear_range=0.2,
+                                   zoom_range=0.2,
+                                   horizontal_flip=True)
 
-test_datagen = ImageDataGenerator(rescale = 1./255)
+test_datagen = ImageDataGenerator(rescale=1. / 255)
 
 # Define the directories for the training and test sets
 train_set = train_datagen.flow_from_directory('train',
-                                              target_size = (32, 32),
-                                              batch_size = 32,
-                                              class_mode = 'sparse')
+                                              target_size=(32, 32),
+                                              batch_size=32,
+                                              class_mode='sparse')
 
 test_set = test_datagen.flow_from_directory('test',
-                                             target_size = (32, 32),
-                                             batch_size = 32,
-                                             class_mode = 'sparse')
+                                            target_size=(32, 32),
+                                            batch_size=32,
+                                            class_mode='sparse')
 
 # Train the model
 history = model.fit(train_set,
-                    epochs = 25,
-                    validation_data = test_set)
+                    epochs=25,
+                    validation_data=test_set)
 
 # Save the model
 model.save("model.h5")
